17b7c10e540bacd673e4c80c83bbc5bff9b82704
David Schneider <david.jay.schneider@gmail.com>
2020-01-16 15:16:06 -0500

    updates to including userName on leaderboard entry

--- models/leaderboardFunctions.js
+++ models/leaderboardFunctions.js
@@ -1,10 +1,26 @@
 const db = require('./connection');
 
+async function getUsernameById(user_id) {
+    const user_name = await db.query(`
+        select name from users where id=$1
+    `, [user_id])
+        .then(data => {
+            // console.log(data);
+            return data
+        })
+        .catch (err => {
+            console.log(err);
+            return [];
+        })
+    // console.log(user_name);
+    return user_name;
+}
+
 async function getTopTenGames() {
     const topGamesArray = await db.query(`
         select * from scores 
         order by score desc
-        limit 10
+        limit 10;
     `)
         .then(data => {
             // console.log(data);
@@ -17,7 +33,28 @@ async function getTopTenGames() {
     return topGamesArray;
 }
 
+async function getTopTotalScores() {
+    const topScoresArray = await db.query(`
+        select user_id, sum(score)
+        from scores
+        group by user_id
+        order by sum desc
+        limit 10;
+    `)
+        .then(data => {
+            console.log(data);
+            return data;
+        })
+        .catch (err => {
+            console.log(err);
+            return [];
+        })
+    return topScoresArray;
+}
+
 module.exports = {
-    getTopTenGames
+    getUsernameById,
+    getTopTenGames,
+    getTopTotalScores
 };
 
--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -4,12 +4,50 @@ var router = express.Router();
 const leaderboard = require('../models/leaderboardFunctions');
 
 router.get('/', async (req, res) => {
+    
+
+
     const topTenGames = await leaderboard.getTopTenGames();
-    console.log(topTenGames);
-    res.json(topTenGames);
-    // res.send(topTenGames);
-    // res.render('leaderboard');
-})
+    for (let entry of topTenGames) {
+        console.log(entry);
+        let userName = await leaderboard.getUsernameById(entry.user_id)
+        console.log(userName);
+        entry.user_id = await userName[0].name;
+        console.log(entry);
+    }
+    // console.log(topTenGames);
+    const topGamesHTML = topTenGames.map((game) => {
+        return `<p>${game.user_id}: ${game.score}</p>`
+    }).join('');
+    
+    const topTotalScores = await leaderboard.getTopTotalScores();
+    for (let entry of topTotalScores) {
+        console.log(entry);
+        let userName = await leaderboard.getUsernameById(entry.user_id)
+        console.log(userName);
+        entry.user_id = await userName[0].name;
+        console.log(entry);
+    }
+    const topScoresHTML = topTotalScores.map((user) => {
+        return `<p>${user.user_id}: ${user.sum}</p>`
+    }).join('');
+    // console.log(topScoresHTML);
+
+    try {
+        res.render('leaderboard', {
+            locals: {
+                topGamesList: topGamesHTML,
+                topScoresList: topScoresHTML
+            }
+        })
+    } catch (e) {
+        res.status(404);
+        res.send('Could not load leaderboards.');
+    }
+
+
+});
+    // consol
 
 module.exports = router;
 
