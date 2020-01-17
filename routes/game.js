cbb902686bcbe1a93b948bfe1fad17cbbb64a749
Bentley Herron <10934614+bentleyherron@users.noreply.github.com>
2020-01-17 15:42:55 -0500

    Moved to function for req

--- routes/game.js
+++ routes/game.js
@@ -6,18 +6,6 @@ var router = express.Router();
 const axios = require('axios').default;
 const jeopardyAPI = 'https://jeopardy.bentleyherron.dev/api';
 
-// Check if user is logged in for user id object for frontend
-if (req.session && req.session.user) {
-        userObj = {
-            user_id: req.session.user.id,
-            username: req.session.user.username
-        }
-    } else {
-        userObj = {
-            user_id: null,
-            username: 'Anonymous'
-        }
-}
 
 async function getQuestionsForRound(showNumber='5392', roundNumber) {
     let array = [];
@@ -36,6 +24,18 @@ async function getQuestionsForRound(showNumber='5392', roundNumber) {
 
 
 router.get('/', async (req, res)=>{
+    // Check if user is logged in for user id object for frontend
+    if (req.session && req.session.user) {
+            userObj = {
+                user_id: req.session.user.id,
+                username: req.session.user.username
+            }
+        } else {
+            userObj = {
+                user_id: null,
+                username: 'Anonymous'
+            }
+    }
     // const data = await data.createArrayofArrayObject('2008-02-05');
     const data = await getQuestionsForRound();
     res.render('game', {
